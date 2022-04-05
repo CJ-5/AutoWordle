24b29ca1aabfa490cf509e216fdc8d53d7cbdb07
cj-5 <2006carter.man@gmail.com>
2022-04-04 20:05:18 -0400

    started main framework

--- lib.py
+++ lib.py
@@ -1,14 +1,17 @@
 import colorama, time, sys, os, json
 from colorama import Style, Fore, Back
+from collections import OrderedDict
+import main
 
 
 def process_action(_action):
     if _action == "analyze":
-        file = input("File Path: ")
-        if os.path.isfile(file):
-            f = open(file)
-            wordlist = json.load(f)["word_list"]
-            f.close()
+        # Analyze the current word list
+        file = None
+        if not main.word_list:
+            print("No wordlist has been loaded... Please load a word list via the 'load' command")
+        else:
+            wordlist = main.word_list
 
             # Run Analysis Script
             letter_pop = {'a': 0,
@@ -48,14 +51,147 @@ def process_action(_action):
             for i in range(len(letter_pop)):
                 print(f"{key_list[i]}: {letter_pop[key_list[i]]}")
 
-            # Sort to List
+            # Sort
             pop_sorted = {key: value for key, value in sorted(letter_pop.items(), key=lambda _value: _value[1])}
-            print("Letters that appeared the most: ")
-            print(f"")
+            pop_keys = list(pop_sorted.keys())[::-1]
+            pop_values = list(pop_sorted.values())[::-1]
 
-        else:
-            print(f"{Fore.RED}Error{Fore.RESET}: File Does Not exist")
+            print("Letters that appeared the most: ")
+            # Top ten letters
+            for i in range(len(pop_sorted)):
+                print(f"{pop_keys[i]}: {pop_values[i]}")
+
     elif _action == "complete":
-        pass
-    elif _action == "exit":
+        print("Not Implemented...")
+        time.sleep(1)
+        os.system("cls")
+    elif _action == "exit":  # Exit Program
+        print("Exiting...")
+        time.sleep(0.3)
         sys.exit(-1)
+    elif _action == "load":  # Load a word list
+        file_loaded = False
+        while not file_loaded:
+            os.system("cls")
+            print(f"{Fore.YELLOW}Loading word list...{Fore.RESET}")
+
+            file_path = input("File Path: ")
+
+            if os.path.isfile(file_path):
+                f = open(file_path)
+                main.word_list = json.load(f)["word_list"]
+                f.close()
+                file_loaded = True  # Exit Loop
+            else:  # file does not exist
+                print(f"{Fore.RED}Specified File path does not exist{Fore.RESET}")
+        print(f"{Fore.GREEN}File Loaded{Fore.RESET}")
+        time.sleep(1)
+    elif _action == "suggest":
+        os.system("cls")
+        _stat = True
+        _action = None
+
+        # Add Script to deal with conflicts of letters being in one list and not the other
+        active_list = []  # Known letters (Max of 6)
+        active_pos = {0: None,  # List of positions that we know the letter is in
+                      1: None,
+                      2: None,
+                      3: None,
+                      4: None,
+                      5: None,
+                      6: None}
+
+        while _stat:
+            # Loop Headers
+            os.system("cls")
+            print(f"\n{Fore.BLUE}Running Suggestions Analytics{Fore.RESET}\n")
+            print(f"{Fore.GREEN}Action List:{Fore.RESET} {Fore.YELLOW}"
+                  f"[{Fore.RESET}{', '.join(x for x in list(main.scommands.keys()))}{Fore.YELLOW}]{Fore.RESET}")
+
+            print(f"{Fore.GREEN}Known Letter List: {Fore.YELLOW}"
+                  f"{[''.join(x for x in active_list), 'Empty List'][not active_list]}")
+            print(f"""
+            {Fore.GREEN}Known Position List:
+            {Fore.YELLOW}1{Fore.RESET}: {Fore.GREEN}{active_pos[0]}
+            {Fore.YELLOW}2{Fore.RESET}: {Fore.GREEN}{active_pos[1]}
+            {Fore.YELLOW}3{Fore.RESET}: {Fore.GREEN}{active_pos[2]}
+            {Fore.YELLOW}4{Fore.RESET}: {Fore.GREEN}{active_pos[3]}
+            {Fore.YELLOW}5{Fore.RESET}: {Fore.GREEN}{active_pos[4]}
+            {Fore.YELLOW}6{Fore.RESET}: {Fore.GREEN}{active_pos[5]}
+            """)
+
+
+            # "l_add", "l_clear", "l_set", "p_clear", "p_set", "exit"
+            saction = input(f"{Fore.GREEN}Action:{Fore.BLUE} ")
+
+            if saction not in list(main.scommands.keys()):
+                print("Suggest Analytics Command is invalid. Please choose from the action list")
+            else:  # Command is ok
+                if saction == "l_add":  # Add to the list of known letters
+                    if len(active_list) == 6:
+                        print(f"{Fore.RED}Cant add to list, there are already 6 letters. Please use 'l_clear' to"
+                              f" clear the active list or 'l_set' to set a new list")
+                        time.sleep(3)
+                    else:
+                        print(f"{Fore.GREEN}Adding to list of known letters{Fore.RESET}\n")
+                        letter = input(f"   {Fore.GREEN}>{Fore.YELLOW}:{Fore.RESET} ")
+                        if not len(letter) == 1:
+                            print(f"{Fore.RED}Letter can only be {Fore.YELLOW}1{Fore.RED} in length.{Fore.RED}")
+                        else:  # Add letter to list
+                            print(f"{Fore.GREEN}Added {Fore.YELLOW}{letter}{Fore.GREEN} to the active list!")
+                            active_list.append(letter)
+                            time.sleep(1)
+
+                elif saction == "l_clear":  # Clear the list of known letters
+                    print(f"{Fore.GREEN}Cleared Active List!{Fore.RESET}")
+                    active_list.clear()
+                    time.sleep(1)
+                elif saction == "l_set":    # The list of known letters (not their exact positions)
+                    print(f"{Fore.GREEN}Setting known letters (upto: {Fore.YELLOW}6{Fore.GREEN}){Fore.RESET}\n")
+                    letter_list = input(f"{Fore.GREEN}>{Fore.YELLOW}:{Fore.RESET} ")
+                    os.system("cls")
+                    if len(letter_list) > 6:
+                        print(f"{Fore.RED}List cannot be above 6 letters...{Fore.RESET}")
+                        time.sleep(1.4)
+                    else:
+                        str_format = "".join(x for x in letter_list)
+                        print(f"{Fore.GREEN}Set new list to {Fore.YELLOW}{str_format}{Fore.RESET}")
+                        active_list = [x for x in letter_list]  # lambda go brr
+                elif saction == "p_clear":  # Clear the Position List
+                    print(f"{Fore.GREEN}Cleared Position List!")
+                    time.sleep(1)
+                elif saction == "p_set":    # Set letters into the positions that are known
+                    os.system("cls")
+                    print(f"{Fore.BLUE}Setting Letter Positions")
+                    for i in range(6):
+                        char = input(f"{Fore.GREEN}Position {Fore.YELLOW}{i+1}: ")
+                        active_pos[i] = [None, char][len(char) == 1 and not char == " "]
+
+                    time.sleep(1)
+                elif saction == "s_list":   # Print list of suggested words
+                    print(f"{Fore.GREEN}Generating Suggestion List...{Fore.YELLOW} (This may take a while)\n")
+                    print(f"{Fore.BLUE}Progress{Fore.RESET}:{Fore.YELLOW} 0%")
+
+                    # Generate list of words that contain known letters
+                    word_list = main.word_list
+                    candidates0 = []
+                    for word in word_list:
+                        valid = True
+                        for letter in word:
+                            if letter not in active_list:
+                                valid = False
+                                break
+
+                        if valid:
+                            # Check if letters are in known positions
+                            
+
+
+                elif saction == "help":  # Print Help Command
+                    for list_item in main.scommands:
+                        print(f"{f'{Fore.GREEN}{list_item}{Fore.RESET}:':<30}"
+                              f"{Fore.YELLOW}{main.scommands[list_item]}{Fore.RESET}")
+                    print("\n\n")
+                elif saction == "exit":
+                    print(Fore.RESET)
+                    _stat = False
--- main.py
+++ main.py
@@ -1,15 +1,38 @@
-import colorama, json, lib, pynput, time
+import colorama, json, lib, time, pynput
 from os import system
+import os
 from colorama import Style, Fore, Back
+import json
 
 colorama.init() # Initiate colorama lib (needed for escape codes)
-action_list = ["analyze", "complete", "exit"]
+action_list = ["analyze", "complete", "exit", "load", "suggest"]
+# scommand_list = ["l_add", "l_clear", "l_set", "p_clear", "p_set", "s_list", "help", exit]
+scommands = {"l_add": "Add to the list of known letters",
+             "l_clear": "Clear the known list of letters [Clears List]",
+             "l_set": "Set the known list of letters (but not their positions) [Clears List]",
+             "p_clear": "Clear the known positions [Clears List]",
+             "p_set": "Set the known positions of letters",
+             "s_list": "Get a list of suggested words",
+             "help": "View this list",
+             "exit": "Exit suggestion mode"}
 action = None
+word_list = []
 
 if __name__ == '__main__':
     print("Initiating WordleAuto...")
     time.sleep(1)
-    while (action is None) or (action not in action_list):
+    print("Loading Word list...")
+
+    if os.path.exists("./data.json"):  # Load words from word list file
+        f = open("./data.json")
+        json_file = json.load(f)
+        word_list = json_file["word_list"]
+        f.close()
+
+    print("Loaded Word List!")
+    # (action is None) or (action not in action_list)
+
+    while True:  # Command Handler
         print("\n\nAction List:")
         print(f"[{', '.join([x for x in action_list])}]")
         action = input("\nPlease input the action to take: ")
@@ -20,3 +43,4 @@ if __name__ == '__main__':
             time.sleep(2)
         else:
             lib.process_action(action)
+
